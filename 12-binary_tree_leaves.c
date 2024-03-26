b1e4fcbf68c0fe33b78d79018bdd4b51ade43f9c
sarahwonago <sarahshaaleh@gmail.com>
2024-03-26 19:41:23 +0300

    leaves nodes

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Counts the leaves in a binary tree
+ * @tree: Tree to count the leaves from
+ *
+ * Return: Number of leaves
+ *         0 if tree is NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
