8f5b6cc3691608eb64400002fe21c5724c444f18
sarahwonago <sarahshaaleh@gmail.com>
2024-03-27 10:59:57 +0300

    binary tree is leaf

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_node - Creates a new binary tree node
- * @parent: Pointer to the parent node of the node to create
+ * @parent: Pointer to the parent node 
  * @value: Value to put in the new node
  *
  * Return: Pointer to the newly created node, or NULL on failure
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Inserts a node as the right-child of another node
- * @parent: Pointer to the node to insert the right-child in
+ * binary_tree_insert_right - Inserts a node as the right-child 
+ * @parent: Pointer to the node to insert the right-child 
  * @value: Value to store in the new node
  *
  * Return: Pointer to the newly created node
